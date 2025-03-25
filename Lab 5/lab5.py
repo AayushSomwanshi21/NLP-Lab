789476aa8fbd56f98d63d9fef7c11454aa01df63
AayushSomwanshi21 <aayushsomwanshi21@gmail.com>
2025-03-25 14:30:15 +0530

    Lab 5

--- Lab 5/lab5.py
+++ Lab 5/lab5.py
@@ -1,4 +1,5 @@
 # Remove digits from sentence using Greedy tokenizer
+# Additional Q1,Q2
 
 import re
 import nltk
@@ -7,10 +8,17 @@ text = "Hello, I have 2 apples and 3 bananas."
 
 tokens = text.split()
 output = []
+count = 0
+digits = []
 
 for token in tokens:
 
     if not re.search(r'\d', token):
         output.append(token)
+    else:
+        count += 1
+        digits.append(token)
+
 
 print(' '.join(output))
+print(f'Digits: {digits} Number of digits: {count}')
---
+++ Lab 5/lab5c.py
@@ -0,0 +1,25 @@
+# Additional Q3
+
+import re
+import nltk
+
+text = "Contact me at abc.mahe@manipal.edu before 02/28/2025"
+
+tokens = text.split()
+print(tokens)
+cur_str = ''
+output = []
+
+for token in tokens:
+
+    if re.search(r'(0[1-9]|[12][0-9]|3[01]/0[1-9]|1[0-2]/\d{4})', token) or re.search(r'([a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,})', token):
+        output.append(cur_str.strip())
+        cur_str = ''
+        output.append(token)
+    else:
+        cur_str += token + ' '
+
+if cur_str:
+    output.append(cur_str)
+
+print(output)
